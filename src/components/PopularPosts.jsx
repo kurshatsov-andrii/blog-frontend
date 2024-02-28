c30df1bb0788c09617e40e79f4efc9b18ee8e272
Andrii Kurshatsov <andreswebit@ukr.net>
2024-02-28 17:13:28 +0200

    Update path popular posts

--- src/components/PopularPosts.jsx
+++ src/components/PopularPosts.jsx
@@ -5,7 +5,7 @@ export const PopularPosts = ({ post }) => {
 	return (
 		<div className='bg-gray-600 my-1'>
 			<Link
-				to={`${post._id}`}
+				to={`/${post._id}`}
 				className='flex text-xs p-2 text-gray-300 hover:bg-gray-800 hover:text-white'
 			>
 				{post.title}
